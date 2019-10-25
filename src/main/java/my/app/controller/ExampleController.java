ef942675deae7c4faad496eb9f9d8e443ef1bf4e
Cliff Workman <cworkman@eborkdev.com>
2019-10-25 01:09:20 -0700

    Added changes to demonstrate incorrect response serialization in lambda runtime using the wrapper when controllers return HttpResponse objects

--- src/main/java/my/app/controller/ExampleController.java
+++ src/main/java/my/app/controller/ExampleController.java
@@ -1,7 +1,13 @@
 package my.app.controller;
 
+import io.micronaut.function.aws.proxy.MicronautAwsProxyResponse;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.Status;
 import lombok.extern.slf4j.Slf4j;
 import my.app.dto.Pong;
 
@@ -10,7 +16,21 @@ import my.app.dto.Pong;
 public class ExampleController {
 
     @Get("/ping")
+    @Produces(MediaType.APPLICATION_JSON)
     public Pong index() {
         return Pong.builder().pong(true).graal(true).build();
+    }
+
+    @Get("/pong")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Status(HttpStatus.CREATED)
+    public HttpResponse<?> withResponseClass() {
+        return HttpResponse.created(Pong.builder().pong(true).graal(true).build());
+    }
+
+    @Get("/error")
+    @Produces(MediaType.APPLICATION_JSON)
+    public HttpResponse<?> withSyntheticError() {
+        throw new RuntimeException("I can't even right now");
     }
 }
